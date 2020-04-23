d0385948265e118ef3f9a4381b3c3eff813ef8f3
Andrey Loginov <andorus911@gmail.com>
2020-04-23 23:37:09 +0300

    map Sorting

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/url"
 
+	"sort"
 	"strconv"
 	"strings"
 	"unicode"
@@ -21,6 +22,17 @@ type Page struct {
 	Text  string
 	Table map[string]int
 }
+
+type Pair struct {
+	Key   string
+	Value int
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
 func (p *Page) toCount() {
 	f := func(c rune) bool {
@@ -51,6 +63,17 @@ func downloadPage(siteUrl string) (*Page, error) {
 	return &Page{Text: string(body)}, nil
 }
 
+func rankByWordCount(wordFrequencies map[string]int) PairList {
+	pl := make(PairList, len(wordFrequencies))
+	i := 0
+	for k, v := range wordFrequencies {
+		pl[i] = Pair{k, v}
+		i++
+	}
+	sort.Sort(sort.Reverse(pl))
+	return pl
+}
+
 func readSiteHandler(w http.ResponseWriter, r *http.Request) {
 	siteUrl := r.URL.Path[len(READ_URL):]
 	p, _ := downloadPage(siteUrl)
@@ -66,9 +89,18 @@ func readSiteHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	// отправить пдф обратно
+	sortedPairs := rankByWordCount(p.Table)
+	// TODO отправить пдф обратно
 
-	fmt.Fprintf(w, "<h1>Table</h1><div>%s</div>", p.Table)
+	fmt.Fprintf(w, "<h1>Table</h1><table>")
+
+	for count, pair := range sortedPairs {
+		fmt.Fprintf(w, "<tr><td>%d</td><td>%s</td></tr>", pair.Value, pair.Key)
+		if count == 10 { // TODO сделать гибче
+			break
+		}
+	}
+	fmt.Fprintf(w, "</table>")
 }
 
 func main() {
